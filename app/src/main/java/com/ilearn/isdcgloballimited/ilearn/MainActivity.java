6444a750d542ce63a807656c45787fd04c616772
shubhs2060 <shubhamsrivastava2050>
2018-04-20 12:49:26 +0530

    testing2

--- app/src/main/java/com/ilearn/isdcgloballimited/ilearn/MainActivity.java
+++ app/src/main/java/com/ilearn/isdcgloballimited/ilearn/MainActivity.java
@@ -1,14 +1,18 @@
 package com.ilearn.isdcgloballimited.ilearn;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +26,26 @@ public class MainActivity extends AppCompatActivity {
 
         //TextView txtTest = (TextView) findViewById(R.id.logoText);
         //txtTest.setText(Html.fromHtml("This is <font color='#ff4343'>Red</font> Color!"));
+
+        Button search = (Button) findViewById(R.id.search_btn);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText word = (EditText) findViewById(R.id.search_bar);
+                String keyword = word.getText().toString();
+
+                if(TextUtils.isEmpty(keyword)) {
+                    word.setError("Please enter a keyword to search");
+                }else{
+                    Intent intent = new Intent(getBaseContext(), ResultActivity.class);
+                    intent.putExtra("Keyword", keyword);
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+
     }
 
     @Override
